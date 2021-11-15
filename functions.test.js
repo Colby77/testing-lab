03326c8f54ef2030ce4f984052b8d645eaf3e1f9
Colby Workman <cgw2319@gmail.com>
2021-11-15 16:04:31 -0700

    added more math functions

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 // const { TestWatcher } = require('@jest/core')
-const {returnTwo, greeting, add} = require('./functions.js')
+const {returnTwo, greeting, add, multiply, divide, subtract} = require('./functions.js')
 
 test('test returntwo to get 2', () => {
     expect(returnTwo()).toEqual(2)
@@ -13,10 +13,29 @@ test('Another test for greeting',() => {
     expect(greeting('Jill')).toEqual('Hello, Jill.')
 })
 
-
-test('test for add',() => {
-    expect(add(1,2)).toEqual(3)
-})
-test('test for add again', () => {
-    expect(add(5,9)).toEqual(14)
+describe('math functions', () => {
+    test('test for add',() => {
+        expect(add(1,2)).toEqual(3)
+    })
+    test('test for add again', () => {
+        expect(add(5,9)).toEqual(14)
+    })
+    test('multiply test1', () => {
+        expect(multiply(3,2)).toEqual(6);
+    })
+    test('multiply test2', () => {
+        expect(multiply(5,2)).toEqual(10)
+    })
+    test('divide test1', () => {
+        expect(divide(10,5)).toEqual(2)
+    })
+    test('divide test2', () => {
+        expect(divide(8,2)).toEqual(4)
+    })
+    test('subract test1', () => {
+        expect(subtract(100,10)).toEqual(90)
+    })
+    test('subtract test2', () => {
+        expect(subtract(43,1)).toEqual(42)
+    })
 })
